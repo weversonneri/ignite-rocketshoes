987c06cf68885903e1612948eab62e2da533012e
Weverosn Neri <weversonneri@outlook.com>
2021-11-19 18:37:11 -0300

    finish header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,26 +7,26 @@ import { Container, Cart } from './styles';
 import { useCart } from '../../hooks/useCart';
 
 const Header = (): JSX.Element => {
-  // const { cart } = useCart();
-  // const cartSize = // TODO;
+	const { cart } = useCart();
+	const cartSize = cart.length;
 
-  return (
-    <Container>
-      <Link to="/">
-        <img src={logo} alt="Rocketshoes" />
-      </Link>
+	return (
+		<Container>
+			<Link to='/'>
+				<img src={logo} alt='Rocketshoes' />
+			</Link>
 
-      <Cart to="/cart">
-        <div>
-          <strong>Meu carrinho</strong>
-          <span data-testid="cart-size">
-            {/* {cartSize === 1 ? `${cartSize} item` : `${cartSize} itens`} */}
-          </span>
-        </div>
-        <MdShoppingBasket size={36} color="#FFF" />
-      </Cart>
-    </Container>
-  );
+			<Cart to='/cart'>
+				<div>
+					<strong>Meu carrinho</strong>
+					<span data-testid='cart-size'>
+						{cartSize === 1 ? `${cartSize} item` : `${cartSize} itens`}
+					</span>
+				</div>
+				<MdShoppingBasket size={36} color='#FFF' />
+			</Cart>
+		</Container>
+	);
 };
 
 export default Header;
