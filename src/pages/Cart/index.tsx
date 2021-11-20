62098b76f2a86f3e27942beb875633128860564e
Weverosn Neri <weversonneri@outlook.com>
2021-11-20 13:42:26 -0300

    format cart data n map ncomponent

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -5,8 +5,8 @@ import {
   MdRemoveCircleOutline,
 } from 'react-icons/md';
 
-// import { useCart } from '../../hooks/useCart';
-// import { formatPrice } from '../../util/format';
+import { useCart } from '../../hooks/useCart';
+import { formatPrice } from '../../util/format';
 import { Container, ProductTable, Total } from './styles';
 
 interface Product {
@@ -18,17 +18,13 @@ interface Product {
 }
 
 const Cart = (): JSX.Element => {
-  // const { cart, removeProduct, updateProductAmount } = useCart();
+	const { cart, removeProduct, updateProductAmount } = useCart();
 
-  // const cartFormatted = cart.map(product => ({
-  //   // TODO
-  // }))
-  // const total =
-  //   formatPrice(
-  //     cart.reduce((sumTotal, product) => {
-  //       // TODO
-  //     }, 0)
-  //   )
+	const cartFormatted = cart.map((product) => ({
+		...product,
+		priceFormatted: formatPrice(product.price),
+		subTotal: formatPrice(product.amount * product.price),
+	}));
 
   function handleProductIncrement(product: Product) {
 		updateProductAmount({ productId: product.id, amount: product.amount + 1 });
@@ -47,21 +43,22 @@ const Cart = (): JSX.Element => {
       <ProductTable>
         <thead>
           <tr>
-            <th aria-label="product image" />
+						<th aria-label='product image' />
             <th>PRODUTO</th>
             <th>QTD</th>
             <th>SUBTOTAL</th>
-            <th aria-label="delete icon" />
+						<th aria-label='delete icon' />
           </tr>
         </thead>
         <tbody>
-          <tr data-testid="product">
+					{cartFormatted.map((product) => (
+						<tr key={product.id} data-testid='product'>
             <td>
-              <img src="https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg" alt="Tênis de Caminhada Leve Confortável" />
+								<img src={product.image} alt={product.title} />
             </td>
             <td>
-              <strong>Tênis de Caminhada Leve Confortável</strong>
-              <span>R$ 179,90</span>
+								<strong>{product.title}</strong>
+								<span>{product.priceFormatted}</span>
             </td>
             <td>
               <div>
@@ -89,18 +86,19 @@ const Cart = (): JSX.Element => {
               </div>
             </td>
             <td>
-              <strong>R$ 359,80</strong>
+								<strong>{product.subTotal}</strong>
             </td>
             <td>
               <button
-                type="button"
-                data-testid="remove-product"
-              // onClick={() => handleRemoveProduct(product.id)}
+									type='button'
+									data-testid='remove-product'
+									onClick={() => handleRemoveProduct(product.id)}
               >
                 <MdDelete size={20} />
               </button>
             </td>
           </tr>
+					))}
         </tbody>
       </ProductTable>
 
